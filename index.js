0943e9b14417fcce601ed39530f129f2842d033c
upRos3 <josh@upros3.com>
2018-04-24 23:49:29 -0400

    Added times visited to localstorage and prime number exercise to index.js

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
+  var timesVisited = 0;
+
+  if (localStorage.timesVisited === null) {
+    localStorage.setItem("timesVisited", 1);
+  } else {
+    var timesVisited = Number(localStorage.getItem("timesVisited"));
+    timesVisited += 1;
+    localStorage.setItem("timesVisited", timesVisited);
+  }
+
+  function isPrime(num) {
+    if (num === 1) {
+      return timesVisited;
+    } else if (num === 2) {
+      return "PRIME!";
+    } else {
+      for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+          return timesVisited;
+        }
+      }
+      return "PRIME!";
+    }
+  }
+
+  function numberOfViewsToday(timesVisited) {
+    return isPrime(timesVisited);
+  }
+
   var $searchButton = $("#search-btn");
   var $menuButton = $("#menu-btn");
 
@@ -12,4 +41,6 @@ $(document).ready(function() {
   $menuButton.on("click", function() {
     console.log("menu");
   });
+
+  console.log(numberOfViewsToday(timesVisited));
 });
